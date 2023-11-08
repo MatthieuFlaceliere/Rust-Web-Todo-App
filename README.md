6fab24bcd628178b8a76d51af6a8e399568db66c
Matthieu Flce <flacelierematthieu@gmail.com>
2023-11-08 23:38:51 +0100

    finish v1

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Rust-Web-Todo-App
+
+This is a simple web todo app written in Rust from scratch. Whitout any framework or library.
+
+## Features
+
+-   [x] Add todo
+-   [x] Remove todo
+
+## Dependencies
+
+-   [lazy_static](https://crates.io/crates/lazy_static) 1.4.0
+
+## How to run
+
+```bash
+$ cargo run
+```
+
+TODO App will be available at http://localhost:8080/
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,71 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
-use std::fs;
+use std::sync::Mutex;
+#[macro_use]
+extern crate lazy_static;
+
+const TOP_HTML: &str = 
+"<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <title>Todo</title>
+    <style>
+        span {
+            display: inline-block;
+            width: 200px;
+            text-align: left;
+        }
+        form {
+            margin: 4px 0;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>To-Do</h1>";
+
+const NO_TODOS_HTML: &str =
+"<p>No todos yet</p>";
+
+const TODO_HTML_START: &str =
+"<form action='/delete_todo' method='POST'>
+    <input type='submit' value='X' />
+    <span>";
+
+const TODO_HTML_MIDDLE: &str =
+"</span>
+    <input type='hidden' name='id' value='";
+
+const TODO_HTML_END: &str =
+"' />
+</form>";
+
+const FORM_HTML: &str =
+"<form action='/add_todo' method='POST'>
+    <input type='text' name='todo' />
+    <input type='submit' value='Add' />
+</form>";
+
+const END_HTML: &str =
+"</body>
+
+</html>";
+
+struct Todo {
+    id: i32,
+    text: String,
+}
+
+lazy_static! {
+    static ref TODOS: Mutex<Vec<Todo>> = Mutex::new(Vec::new());
+}
 
 fn main() {
-    let listener = match TcpListener::bind("127.0.0.1:4221"){
+    let listener = match TcpListener::bind("127.0.0.1:8080"){
         Ok(listener) => listener,
         Err(e) => {
             println!("Error: {}", e);
@@ -36,48 +97,19 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
     let request_str = String::from_utf8_lossy(&buffer[..]);
 
-    if request_str.contains("GET") {
-        get_request(&stream, &request_str);
-    } else if request_str.contains("POST") {
-        post_request(&stream, &request_str);
-    } else {
-        println!("Error: invalid request");
-        match stream.write("HTTP/1.1 400 Bad Request\r\n\r\n".as_bytes()) {
-            Ok(_) => println!("Response sent: \n{:?}", "HTTP/1.1 400 Bad Request\r\n\r\n"),
-            Err(e) => println!("Failed sending response: {}", e),
-        }
-    }
-}
-
-fn get_request(mut stream: &TcpStream, request_str: &str) {
-    let lines: Vec<&str> = request_str.split("\r\n").collect();
-    let first_line = lines[0];
-    let path: Vec<&str> = first_line.split(" ").collect();
-    let pathname = path[1];
-
-    let response = match pathname {
-        _task_manager if pathname.contains("/task-manager") || pathname == "/" => {
-            println!("GET {}", pathname);
-
-            match fs::read_to_string("src/task-manager/index.html") {
-                Ok(contents) => {
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", contents.len(), contents)
-                }
-
-                Err(e) => {
-                    println!("Error: {}", e);
-                    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
-                }
-
-            }
-        }
-        _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+    let response = match request_str {
+        _ if request_str.contains("GET") => get_request(&request_str),
+        _ if request_str.contains("POST") => post_request(&request_str),
+        _ => {
+            println!("Error: invalid request");
+            "HTTP/1.1 400 Bad Request\r\n\r\n".to_string()
+        },
     };
 
     match stream.write(response.as_bytes()) {
@@ -86,7 +118,33 @@ fn get_request(mut stream: &TcpStream, request_str: &str) {
     }
 }
 
-fn post_request(mut stream: &TcpStream, request_str: &str) {
+fn get_request(request_str: &str) -> String {
+    let lines: Vec<&str> = request_str.split("\r\n").collect();
+    let first_line = lines[0];
+    let path: Vec<&str> = first_line.split(" ").collect();
+    let pathname = path[1];
+    
+    println!("GET {}", pathname);
+
+    match pathname {
+        _get_todo if pathname == "/" => get_todos_request(),
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n404 Page not found\r\n\r\n".to_string(),
+    }
+}
+
+fn get_todos_request() -> String {
+    let todos = match TODOS.lock() {
+        Ok(todos) => todos,
+        Err(e) => {
+            println!("Error: {}", e);
+            return "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string();
+        }
+    };
+    
+    create_http_response(&todos)
+}
+
+fn post_request(request_str: &str) -> String {
     let lines: Vec<&str> = request_str.split("\r\n").collect();
     let first_line = lines[0];
     let path: Vec<&str> = first_line.split(" ").collect();
@@ -96,16 +154,92 @@ fn post_request(mut stream: &TcpStream, request_str: &str) {
         None => "",
     };
 
-    let response = match pathname {
-        _files if pathname.contains("/files") => {
-            println!("{}", body);
-            "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+    println!("POST {}", pathname);
+
+    match pathname {
+        _add_todo if pathname.contains("/add_todo") => {
+            add_todos_request(&body)
+        },
+        _delete_todo if pathname.contains("/delete_todo") => {
+            delete_todos_request(&body)
         },
         _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
-    };
-
-    match stream.write(response.as_bytes()) {
-        Ok(_) => println!("Response sent: \n{:?}", response),
-        Err(e) => println!("Failed sending response: {}", e),
     }
 }
+
+fn add_todos_request(body : &str) -> String {
+    // Get todos
+    let mut todos = match TODOS.lock() {
+        Ok(todos) => todos,
+        Err(e) => {
+            println!("Error: {}", e);
+            return "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string();
+        }
+    };
+
+    // Create new todo
+    let id = todos.len() as i32;
+    let body_split: Vec<&str> = body.split("=").collect();
+
+    let todo = Todo {
+        id,
+        text: body_split[1].to_string(),
+    };
+    todos.push(todo);
+
+    create_http_response(&todos)    
+}
+
+fn delete_todos_request(body: &str) -> String {
+    // Get todos
+    let mut todos = match TODOS.lock() {
+        Ok(todos) => todos,
+        Err(e) => {
+            println!("Error: {}", e);
+            return "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string();
+        }
+    };
+
+    // Delete todo
+    let body_split: Vec<&str> = body.split("=").collect();
+    let id = match body_split[1].parse::<i32>() {
+        Ok(id) => id,
+        Err(e) => {
+            println!("Error: {}", e);
+            return "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string();
+        }
+    };
+    todos.retain(|todo| todo.id != id);
+
+    create_http_response(&todos)
+}
+
+fn create_http_response(todos: &Vec<Todo>) -> String {
+    // Create HTML for todos
+    let mut todos_html = String::new();
+    for todo in todos.iter() {
+        let mut todo_html: String = String::new();
+        todo_html.push_str(TODO_HTML_START);
+        todo_html.push_str(&todo.text);
+        todo_html.push_str(TODO_HTML_MIDDLE);
+        todo_html.push_str(&todo.id.to_string());
+        todo_html.push_str(TODO_HTML_END);
+
+        todos_html.push_str(&todo_html);
+    }
+
+    // Create response
+    let mut response = String::new();
+    response.push_str("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
+    response.push_str(TOP_HTML);
+    if todos_html.is_empty() {
+        response.push_str(NO_TODOS_HTML);
+    } else {
+        response.push_str(&todos_html);
+    }
+    response.push_str(FORM_HTML);
+    response.push_str(END_HTML);
+
+    response
+}
+
